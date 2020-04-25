0030eff8758de7adb76698495c5c12ed15992a72
VladBohdan <darekz04111995@gmail.com>
2020-04-25 15:14:49 +0300

    dinamicForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,27 +7,45 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  form: FormGroup;
+  form: FormGroup
 
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl('', [
-          Validators.email,
-          Validators.required
+        Validators.email,
+        Validators.required
       ]),
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
-      ])
-    });
+      ]),
+      address: new FormGroup({
+        country: new FormControl('by'),
+        city: new FormControl('Минск', Validators.required)
+      })
+    })
   }
 
   submit() {
     if (this.form.valid) {
-      console.log('Form : ', this.form);
-      const formData = {...this.form.value};
-      console.log('Form Data: ', formData);
+      console.log('Form: ', this.form)
+      const formData = {...this.form.value}
+
+      console.log('Form Data:', formData)
     }
+  }
+
+  setCapital() {
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    }
+
+    const cityKey = this.form.get('address').get('country').value
+    const city = cityMap[cityKey]
+
+    this.form.patchValue({address: {city}})
   }
 }
 
